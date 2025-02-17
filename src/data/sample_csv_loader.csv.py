8e7b242fe50ffb9e95f95122903992ebbf5f571b
dkrasne <dkrasne@gmail.com>
2025-02-17 10:31:12 -0500

    Fix script path

--- src/data/sample_csv_loader.csv.py
+++ src/data/sample_csv_loader.csv.py
@@ -1,7 +1,11 @@
 import sys
 
 import pandas as pd
+import os
 
-csv = pd.read_csv("sample_csv.csv")
+scriptdir = os.path.dirname(os.path.abspath(__file__))
+
+csv = pd.read_csv(f"{scriptdir}/sample_csv.csv")
+print(csv.head())
 
 csv.to_csv(sys.stdout)
