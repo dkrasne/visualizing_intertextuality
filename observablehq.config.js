595eeedd31166763a288e7371bb67bd5ddd6f724
dkrasne <dkrasne@gmail.com>
2025-06-15 09:36:05 -0400

    Modify GitHub Pages deploy to use GitHub Actions

--- observablehq.config.js
+++ observablehq.config.js
@@ -37,8 +37,8 @@ export default {
   // sidebar: true, // whether to show the sidebar
   // toc: true, // whether to show the table of contents
   // pager: true, // whether to show previous & next links in the footer
-  // output: "dist", // path to the output root for build
-  output: "docs", // recommended for GitHub pages.
+  output: "dist", // path to the output root for build
+  // output: "docs", // recommended for GitHub pages.
   // search: true, // activate search
   // linkify: true, // convert URLs in Markdown to links
   // typographer: false, // smart quotes and other typographic improvements
